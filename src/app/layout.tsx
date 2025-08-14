cfadfce66d1fc5c1c5cecc5c54537d469520dd71
Lahcen Ait el kaid <Yokaitelkaid@gmail.com>
2025-08-14 21:26:01 +0000

    j'ai tester mais aucun icon s'affiche jsut une lettre C en couleur verte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Aref+Ruqaa:wght@400;700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#B2D7B3" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className="font-body antialiased h-full">
         {children}
