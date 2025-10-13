c6bcf67c4b8e66798dd6b992bb60d45c1883e4f7
Lahcen Ait el kaid <Yokaitelkaid@gmail.com>
2025-10-13 09:19:17 +0000

    i want to collecg dataa by instaling wercel speed insight can you help m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Cargo',
@@ -42,6 +43,7 @@ export default function RootLayout({
       <body className="font-body antialiased h-full">
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
